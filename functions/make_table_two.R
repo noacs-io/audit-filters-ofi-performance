74a48893e379bcef7159d57bb3ec66a57ca21d5d
JosefAl-Khalili <josefa@k9tbt01.ki.se>
2023-12-14 22:04:22 +0100

    update

--- functions/make_table_two.R
+++ functions/make_table_two.R
@@ -1,8 +1,8 @@
-make_table_two <- function(calculateSensSpec,confidenceInterval, calculateAUC){
+make_table_two <- function(calculateSensSpec,confidenceInterval, calculateAUC, listOfAuditFiltersClean, counter){
   sensitivityResultFinal <- paste(c(round(calculateSensSpec[1] * 100, digits = 1)," (",confidenceInterval[1],"-",confidenceInterval[2],") "), collapse = "")
   specificityResultFinal <- paste(c(round(calculateSensSpec[2] * 100, digits = 1)," (",confidenceInterval[3],"-",confidenceInterval[4],") "), collapse = "")
   numberOfTrue <- calculateSensSpec[3]
-  tableResult <- data.frame(Auditfilter = auditFilter,
+  tableResult <- data.frame(Auditfilter = listOfAuditFiltersClean[counter],
                             Number = numberOfTrue,
                             Specificity = specificityResultFinal,
                             Sensitivity = sensitivityResultFinal)
