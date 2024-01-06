01de27e88f00971c72f0b3f52d1bcfe51187cd41
JosefAl-Khalili <josefa@k9tbt01.ki.se>
2024-01-06 16:53:04 +0100

    update

--- functions/confidence_interval.R
+++ functions/confidence_interval.R
@@ -6,13 +6,19 @@ confidence_interval <- function(twoVariableData){
   AUCCI <- boot.ci(boot2, type = "basic")
   ## Implement "bootstrap p-value"
   mu <- 0.5
+  mu0.8 <- 0.8
   test.statistic <- abs(boot2$t0 - mu)
   test.distribution <- boot2$t - mu
   centered.test.distribution <- test.distribution - mean(test.distribution)
   p.value <- mean(centered.test.distribution < -test.statistic | centered.test.distribution > test.statistic)
   ## t.test
+  test.statistic0.8 <- abs(boot2$t0 - mu0.8)
+  test.distribution0.8 <- boot2$t - mu0.8
+  centered.test.distribution0.8 <- test.distribution0.8 - mean(test.distribution0.8)
+  p.value0.8 <- mean(centered.test.distribution0.8 < -test.statistic0.8 | centered.test.distribution0.8 > test.statistic0.8)
+  ## t.test
   tTest <- t.test(boot2$t, alternative = "two.sided", mu = 0.5)
   sensitivityCI <- boot.ci(bootResults, type = "basic", index = 1)  # Sensitivity CI
   specificityCI <- boot.ci(bootResults, type = "basic", index = 2) # Specificity CI
-  return(c(round(sensitivityCI$basic[4:5] * 100, digits = 1), round(specificityCI$basic[4:5] * 100, digits = 1), round(AUCCI$basic[4:5], digits = 3), round(p.value, digits = 3)))
+  return(c(round(sensitivityCI$basic[4:5] * 100, digits = 1), round(specificityCI$basic[4:5] * 100, digits = 1), round(AUCCI$basic[4:5], digits = 3), round(p.value, digits = 3), round(p.value0.8, digits = 3)))
 }
--- functions/make_table_three.R
+++ functions/make_table_three.R
@@ -2,7 +2,8 @@ make_table_three <- function(calculateAUC, confidenceInterval, listOfAuditFilter
   AUCResultFinal <- paste(c(round(calculateAUC, digits = 2)," (",confidenceInterval[5],"-",confidenceInterval[6],") "), collapse = "")
   tableResult <- data.frame(Auditfilter = listOfAuditFiltersClean[counter],
                             AUC = AUCResultFinal,
-                            PValue = ifelse(confidenceInterval[7] == 0, "<0.0001", as.character(confidenceInterval[7])) )
+                            PValue = ifelse(confidenceInterval[7] == 0, "<0.0001", as.character(confidenceInterval[7])),
+                            PValue0.8 = ifelse(confidenceInterval[8] == 0, "<0.0001", as.character(confidenceInterval[7])))
 
   return(tableResult)
 }
